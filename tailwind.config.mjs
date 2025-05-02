4d917052b9313cab59f6eee8c5a8f780f8efdece
constanzagalia <coco.galiam@gmail.com>
2025-05-02 19:34:48 -0300

    Add styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,23 +8,23 @@ export default {
     extend: {
       fontFamily: { // add new font family
         roboto: ['Roboto', 'sans-serif']
+      },
+      colors: { // custom color palette
+        primary: '#003946',
+        secondary: '#00B3AC',
+        success: '#5FC790',
+        warning: '#FFA600',
+        danger: '#FF5630',
+        dark: '#2E3A44',
+        info: '#1CA7EC',
+        black: '#2E3A44',
+        grey1: '#A0AABF',
+        grey2: '#C0C6D4',
+        grey3: '#E3E8F1',
+        light: '#F9FBFC',
+        white: '#FFF'
       }
     },
-    colors: { // custom color palette
-      primary: '#003946',
-      secondary: '#00B3AC',
-      success: '#5FC790',
-      warning: '#FFA600',
-      danger: '#FF5630',
-      dark: '#2E3A44',
-      info: '#1CA7EC',
-      black: '#2E3A44',
-      grey1: '#A0AABF',
-      grey2: '#C0C6D4',
-      grey3: '#E3E8F1',
-      light: '#F9FBFC',
-      white: '#FFF'
-    }
   },
   plugins: [typography],
 };
