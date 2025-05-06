470f2b144ec41abd287dffeedb03da74d9535255
constanzagalia <coco.galiam@gmail.com>
2025-05-06 15:54:48 -0300

    add .astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-supabase-auth.vercel.app",
